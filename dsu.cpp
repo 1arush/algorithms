44bbff6238e5254245aed9a149c67d7e843c868a
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-01-01 09:43:42 +0530

    Update dsu.cpp
    Struct-based implementation

--- dsu.cpp
+++ dsu.cpp
@@ -1,32 +1,20 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-const int N=1e6+1;
-vector<int> parent[N], rank[N];
-
-void make_set(int v){
-  parent[v]=v;
-  rank[v]=0;
-}
-
-int find_set(int v){
-  if(v==parent[v]) return v;
-  return parent[v]=find_set(parent[v]);
-}
-
-void unite_sets(int a, int b){
-  a=find_set(a);
-  b=find_set(b);
-  if(a!=b){
-    if(rank[a]<rank[b]) swap(a,b);
-    parent[b]=a;
-    if(rank[a]==rank[b]) rank[a]++;
-  }
-}
-
-int main(){
-  ios::sync_with_stdio(0), cin.tie(0);
-  
-  // fill main function
-  return 0;
-}
+struct DSU {
+    int n;
+    vector<int> sz, p;
+    DSU(int _n) : n(_n) {
+        sz.assign(n,1), p.resize(n);
+        iota(all(p),0);
+    }
+    int leader(int a){
+        return (a==p[a] ? a : p[a]=leader(p[a]));
+    }
+    bool unite(int a, int b){
+        a=leader(a), b=leader(b);
+        if(sz[a]<sz[b]) swap(a,b);
+        if(a!=b){
+            p[b]=a, sz[a]+=sz[b];
+            return 1;
+        }
+        return 0;
+    }
+};

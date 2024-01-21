c9f6208d12ac53eac0c537feea2dac5d5848431a
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-01-21 23:06:34 +0530

    Updated dijkstra.cpp
    Template

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,53 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
-
-// @author: rushil
-
-#include <bits/stdc++.h>
-using namespace std;
-#define pii pair<int,int>
-
-int n,m;
-vector<vector<pii>>g;
-long long dist[100001];
-int p[100001];
-long long inf=1e18;
-
-int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-
-    cin>>n>>m;
-    fill(dist,dist+n+1,inf);
-    fill(p,p+n+1,-1);
-    g.resize(n+2);
-    for(int i=0;i<m;i++){
-        int x,y,d; cin>>x>>y>>d;
-        g[x].push_back({y,d});
-        g[y].push_back({x,d});
-    }
-    set<pii>q;
-    dist[1]=0;
-    q.insert({0,1});
-    while(!q.empty()){
-        int v=q.begin()->second;
-        q.erase(q.begin());
-        for(auto t:g[v]){
-            long long u=t.first;
-            long long w=t.second;
-            if(dist[v]+w<dist[u]){
-                q.erase({dist[u],u});
-                dist[u]=dist[v]+w;
-                q.insert({dist[u],u});
-                p[u]=v;
-            }
+vector<int> dist(N,1e18);
+priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+pq.emplace(0,start), dist[start]=0;
+while(!pq.empty()){
+    auto [d,u]=pq.top();
+    pq.pop();
+    if(d>dist[u]) continue;
+    for(auto [v,w]:gr[u]){
+        if(d+w<dist[v]){
+            dist[v]=d+w;
+            pq.emplace(dist[v],v);
         }
     }
-    if(dist[n]==inf || p[n]==0){ cout<<-1; return 0; }
-    int i=n;
-    vector<int>path;
-    while(p[i]!=-1){
-        path.push_back(i);
-        i=p[i];
-    }
-    path.push_back(1);
-    for(int i=path.size()-1;i>=0;i--) cout<<path[i]<<' ';
 }

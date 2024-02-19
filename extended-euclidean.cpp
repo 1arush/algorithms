263045049ac0e9d8bb435209ee8a05db02797cbd
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-02-19 12:30:08 +0530

    Update extended-euclidean.cpp
    Templated format

--- extended-euclidean.cpp
+++ extended-euclidean.cpp
@@ -1,21 +1,11 @@
-#define _CRT_SECURE_NO_WARNINGS
-#define NDEBUG
-
-// @author: rushil
-
-#include <bits/stdc++.h>
-using namespace std;
-
-tuple<long long,long long,long long> GCD(long long a, long long b){    // returns solution (x,y) and gcd(a,b)
-    if(b==0) return make_tuple(1,0,a);
-    tuple<long long,long long,long long> S=GCD(b, a%b);
-    long long x=get<0> (S), y=get<1> (S), d=get<2> (S);
-    return make_tuple(y, x-(a/b)*y, d);
-}
-int main(){
-    ios::sync_with_stdio(false); cin.tie(NULL);
-
-    long long a, b, c; cin>>a>>b>>c;
-    tuple<long long,long long,long long> t;
-    t=GCD(a, b);
+template<typename T>
+T extgcd(T a, T b, T &x, T &y){
+    if(a==0){
+        x=0, y=1;
+        return b;
+    }
+    T p=b/a;
+    T g=extgcd(b-p*a,a,y,x);
+    x-=p*y;
+    return g;
 }

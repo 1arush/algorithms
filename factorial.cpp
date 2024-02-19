2afb5b5862e123256c0fa9f7890f16889ffea3fd
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-02-19 12:47:15 +0530

    Update factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -7,7 +7,7 @@ void build_combination(){
     fac[0]=fac[1]=inv[0]=inv[1]=f[0]=f[1]=1;
     for(int i=2; i<mxN; i++){
         fac[i]=fac[i-1]*i%MOD;
-        f[i]=(MOD-MOD/i)*f[MOD%i]%MOD;
+        f[i]=(MOD-MOD/i)*f[MOD%i]%MOD;  
         inv[i]=inv[i-1]*f[i]%MOD;
     }
 }

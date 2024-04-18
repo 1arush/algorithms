dc030c4e3e2e6f156e21410e2033ff8446e624f4
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-04-18 16:40:21 +0530

    Added shank-algo.cpp (DLP)
    Shank's algorithm for DLP. Adapted from cryptanalysis lab.

---
+++ shank-algo.cpp
@@ -0,0 +1,47 @@
+int pow(int g, int x, int M){
+        x=(M+x)%M;
+        int res=1;
+        while(x){
+                if(x&1) res=res*g%M;
+                x>>=1, g=g*g%M;
+        }
+        return res;
+}
+
+int shank_algo(int g, int M, int r){
+        // solves for g^x = r (mod M)
+        
+        // g = generator
+        // M = modulus
+        // r = value
+        vector<pair<int,int>> f, s;
+        int m=(int)ceil(sqrtl(M-1));
+        for(int i=0; i<m; ++i){
+                int g_mi=pow(g,m*i,M);
+                f.emplace_back(g_mi,i);
+        }
+        sort(f.begin(),f.end());
+        for(int i=0; i<m; ++i){
+                // since g^(M-1) = 1 (mod M)
+                // we have g^(-i) = g^(M-1-i) (mod M)
+                int g_i=pow(g,(M-1-i),M);
+                s.emplace_back(r*g_i%M,i);
+        }
+        sort(s.begin(),s.end());
+        for(int i=0; i<m; ++i){
+                auto [v,id]=f[i];
+                int L=0, R=m-1;
+                // binary search
+                while(R>L+1){
+                        int MID=(L+R)/2;
+                        if(s[MID].first<=v) L=MID;
+                        else R=MID;
+                }
+                if(s[L+1].first<=v) ++L; 
+                // check answer
+                if(s[L].first==v){
+                        return m*id+s[L].second;
+                }
+        }
+        return -1;
+}

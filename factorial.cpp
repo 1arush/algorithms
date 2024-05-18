31905f4197910f9bc3e3ce82a7130f35962d5f3c
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-18 16:13:06 +0530

    Updated factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,16 +1,16 @@
-const int64_t MOD=1e9+7;  // prime
-const int mxN=200005;
+const int64_t MOD=1e9+7;  // prime moduli
+const int MXN=2e5+5;
 
-vector<int64_t> fac(mxN), inv(mxN), f(mxN);
+vector<int64_t> fac(MXN), inv(MXN), f(MXN);
 
 void build_combination(){
-    fac[0]=fac[1]=inv[0]=inv[1]=f[0]=f[1]=1;
-    for(int i=2; i<mxN; i++){
-        fac[i]=fac[i-1]*i%MOD;
-        f[i]=(MOD-MOD/i)*f[MOD%i]%MOD;  
-        inv[i]=inv[i-1]*f[i]%MOD;
-    }
+        fac[0]=fac[1]=inv[0]=inv[1]=f[0]=f[1]=1;
+        for(int i=2; i<MXN; i++){
+                fac[i]=fac[i-1]*i%MOD;
+                f[i]=(MOD-MOD/i)*f[MOD%i]%MOD;  
+                inv[i]=inv[i-1]*f[i]%MOD;
+        }
 }
 int64_t C(int n, int r){
-    return fac[n]*inv[r]%MOD*inv[n-r]%MOD;
+        return fac[n]*inv[r]%MOD*inv[n-r]%MOD;
 }

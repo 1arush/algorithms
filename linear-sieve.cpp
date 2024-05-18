f764383f708767cd2edc51cd29edc3707f6a6a84
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-18 19:03:25 +0530

    Updated linear-sieve.cpp

--- linear-sieve.cpp
+++ linear-sieve.cpp
@@ -1,12 +1,12 @@
 const int MXN=1e6+6;
 vector<int> lp(MXN), pr;
 
-for(ll i=2; i<=N; ++i){
+for(ll i=2; i<MXN; ++i){
         if(lp[i]==0){
                 lp[i]=i;
                 pr.push_back(i);
         }
-        for(ll j=0; i*pr[j]<=N; ++j){
+        for(ll j=0; i*pr[j]<MXN; ++j){
                 lp[i*pr[j]]=pr[j];
                 if(pr[j]==lp[i]) break;
         }

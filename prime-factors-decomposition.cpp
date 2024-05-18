a065e60923a3dde10358a069e84065899711b140
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-18 19:08:18 +0530

    Updated prime-factors-decomposition.cpp

--- prime-factors-decomposition.cpp
+++ prime-factors-decomposition.cpp
@@ -1,37 +1,24 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll=long long;
-
-vector<pair<ll,int>> prime_factors_decomposition(ll x){
-    vector<pair<ll,int>>f;
-    for(ll i=2; i<=x/i; i++){
-        pair<ll,int> p={i,0};
-        while(x%i==0){
-            x/=i; p.second++;
+vector<pair<int,int>> prime_factors(int x){
+        vector<pair<int,int>> ret;
+        for(int i=2; i*i<=x; ++i){
+                if(x%i) continue;
+                int cnt=0;
+                while(x%i==0){
+                        x/=i, ++cnt;
+                }
+                ret.emplace_back(i,cnt);
         }
-        if(p.second) f.push_back(p);
-    }
-    if(x>1) f.push_back({x,1});
-    return f;
+        if(x>1) ret.emplace_back(x,1);
+        return ret;
 }
 
-vector<ll> generate_divisors(ll x){
-    vector<ll> v;
-    for(ll i=1; i<=x/i; i++){
-        if(x%i==0){
-            v.push_back(i);
-            if(i!=x/i) v.push_back(x/i);
+vector<int> divisors(int x){
+        vector<int> ret;
+        for(int i=1; i*i<=x; ++i){
+                if(x%i==0){
+                        ret.push_back(i);
+                        if(i*i!=x) ret.push_back(x/i);
+                }
         }
-    }
-    return v;
+        return ret;
 }
-
-int main(){
-    ll n; cin>>n;
-    vector<pair<ll,int>> k=prime_factors_decomposition(n);
-    vector<ll> P=generate_divisors(n);
-    for(auto x:P) cout<<x<<' '; cout<<'\n';
-    for(auto x:k){
-        cout<<x.first<<" "<<x.second<<"\n";
-    }
- }

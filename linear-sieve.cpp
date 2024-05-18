46f66e3a0d9ba0e26f01308ff9976897c40a124b
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-18 19:02:43 +0530

    Updated linear-sieve.cpp

--- linear-sieve.cpp
+++ linear-sieve.cpp
@@ -1,21 +1,13 @@
-#include <bits/stdc++.h>
-using namespace std;
-using ll=long long;
-const ll N=1e7;
-vector<ll> lp(N+1,0);
-vector<ll> pr;
+const int MXN=1e6+6;
+vector<int> lp(MXN), pr;
 
-int main(){
-    ios::sync_with_stdio(0); cin.tie(0);
-
-    for(ll i=2; i<=N; i++){
+for(ll i=2; i<=N; ++i){
         if(lp[i]==0){
-            lp[i]=i;
-            pr.push_back(i);
+                lp[i]=i;
+                pr.push_back(i);
         }
-        for(ll j=0; i*pr[j]<=N; j++){
-            lp[i*pr[j]]=pr[j];
-            if(pr[j]==lp[i]) break;
+        for(ll j=0; i*pr[j]<=N; ++j){
+                lp[i*pr[j]]=pr[j];
+                if(pr[j]==lp[i]) break;
         }
-    }
 }

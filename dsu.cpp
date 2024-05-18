f17f38342e6a1c9cc3c2b6a25b2ca63a49aa55c2
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-18 15:54:49 +0530

    Updated dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -1,20 +1,20 @@
 struct DSU {
-    int n;
-    vector<int> sz, p;
-    DSU(int _n) : n(_n) {
-        sz.assign(n,1), p.resize(n);
-        iota(all(p),0);
-    }
-    int leader(int a){
-        return (a==p[a] ? a : p[a]=leader(p[a]));
-    }
-    bool unite(int a, int b){
-        a=leader(a), b=leader(b);
-        if(sz[a]<sz[b]) swap(a,b);
-        if(a!=b){
-            p[b]=a, sz[a]+=sz[b];
-            return 1;
+        int n;
+        vector<int> sz, p;
+        DSU(int _n) : n(_n){
+                sz.assign(n,1), p.resize(n);
+                iota(p.begin(),p.end(),0);
         }
-        return 0;
-    }
+        int leader(int a){
+                return (a==p[a] ? a : p[a]=leader(p[a]));
+        }
+        bool unite(int a, int b){
+                a=leader(a), b=leader(b);
+                if(sz[a]<sz[b]) swap(a,b);
+                if(a!=b){
+                        p[b]=a, sz[a]+=sz[b];
+                        return 1;
+                }
+                return 0;
+        }
 };

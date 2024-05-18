a664936c89a86a12c47b25593dbd26b22cbe9265
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-18 18:59:24 +0530

    Updated bfs.cpp

--- bfs.cpp
+++ bfs.cpp
@@ -1,11 +1,11 @@
 queue<int>q;
 q.push(i), dist[i]=0;
 while(!q.empty()){
-    int u=q.front(); q.pop();
-    for(int v:gr[u]){
-        if(dist[v]==-1){
-            q.push(v);
-            dist[v]=dist[u]+1;
+        int u=q.front(); q.pop();
+        for(int v:gr[u]){
+                if(dist[v]==-1){
+                        q.push(v);
+                        dist[v]=dist[u]+1;
+                }
         }
-    }
 }

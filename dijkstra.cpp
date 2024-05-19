4ab865e8dec18739977e223156ca27236ea45702
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-19 16:26:55 +0530

    Updated dijkstra.cpp

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,15 +1,19 @@
 // graph is weighted, starting vertex is "start"
-vector<int> dist(N,1e18);
-priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
-pq.emplace(0,start), dist[start]=0;
-while(!pq.empty()){
-    auto [d,u]=pq.top();
-    pq.pop();
-    if(d>dist[u]) continue;
-    for(auto [v,w]:gr[u]){
-        if(d+w<dist[v]){
-            dist[v]=d+w;
-            pq.emplace(dist[v],v);
+
+vector<int> dijk(int start){
+        vector<int> dist(n,1e18);  
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<pair<int,int>>> pq;
+        pq.emplace(0,start), dist[start]=0;
+        while(!pq.empty()){
+                auto [d,u]=pq.top();
+                pq.pop();
+                if(d>dist[u]) continue;
+                for(auto [v,w]:gr[u]){
+                        if(d+w<dist[v]){
+                            dist[v]=d+w;
+                            pq.emplace(dist[v],v);
+                        }
+                }
         }
-    }
+        return dist;
 }

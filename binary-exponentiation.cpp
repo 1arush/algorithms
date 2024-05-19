2520451542d89a06e3a03d6ca75a41fe9fd4b8d9
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-19 16:20:10 +0530

    Updated binary-exponentiation.cpp

--- binary-exponentiation.cpp
+++ binary-exponentiation.cpp
@@ -1,9 +1,9 @@
 template <typename T> 
 T binpow(T x, T n, T m=MOD){  // MOD=1e9+7
-	T res=1;
-	while(n){
-		if(n&1) res=res*x%m;
-		x=x*x%m; n>>=1;
-	}
-	return res;
+        T ret=1;
+        while(n){
+                if(n&1) ret=(res*x)%m;
+                x=(x*x)%m; n>>=1;
+        }
+        return ret;
 }

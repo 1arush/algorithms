75252f2bf50281f28c3c8f7db579eece6dc0107a
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-19 16:09:41 +0530

    Updated trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -4,23 +4,23 @@ const int N=2e5*32+500, A=2;
 int nxt[N][A], cnt[N], idx;
 
 void insert(int x, int v){
-  int p=0;
-  for(int i=31; ~i; --i){
-    int u=(x>>i)&1;
-    if(!nxt[p][u]) nxt[p][u]=++idx;
-    cnt[p]+=v, p=nxt[p][u];
-  }
-  cnt[p]+=v;
+        int p=0;
+        for(int i=31; ~i; --i){
+                int u=(x>>i)&1;
+                if(!nxt[p][u]) nxt[p][u]=++idx;
+                cnt[p]+=v, p=nxt[p][u];
+        }
+        cnt[p]+=v;
 }
 
 int query(int x){
-  int p=0, res=0;
-  for(int i=31; ~i; --i){
-    int u=(x>>i)&1;
-    if(nxt[p][u] && cnt[nxt[p][u]]){
-      res+=(1<<i), p=nxt[p][u];
-    }
-    else p=nxt[p][!u];
-  }
-  return res;
+        int p=0, ret=0;
+        for(int i=31; ~i; --i){
+                int u=(x>>i)&1;
+                if(nxt[p][u] && cnt[nxt[p][u]]){
+                        ret+=(1<<i), p=nxt[p][u];
+                }
+                else p=nxt[p][!u];
+        }
+        return ret;
 }

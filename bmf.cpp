285e3a8843bbc735a09cba8588c30c110a46d17e
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-20 13:23:06 +0530

    Updated bmf.cpp

--- bmf.cpp
+++ bmf.cpp
@@ -4,7 +4,7 @@ int n, m;
 vector<array<int,3>> edges;
 
 void read_graph(){
-        for(int i=0; i<m; i++){
+        for(int i=0; i<m; ++i){
                 int a,b,w;
                 cin>>a>>b>>w, --a, --b;
                 array<int,3> arr {a,b,w};
@@ -17,7 +17,7 @@ void read_graph(){
 vector<int> bellman_ford(int x){
         vector<int> dist(n,INF);
         dist[x]=0;
-        for(int i=1; i<n; i++){
+        for(int i=1; i<n; ++i){
                 for(auto e:edges){
                         int a=e[0], b=e[1], w=e[2];
                         dist[b]=min(dist[b],dist[a]+w);

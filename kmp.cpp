44a531077a7d214e7455b59b2980705bc9e38fbf
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-20 13:22:40 +0530

    Updated kmp.cpp

--- kmp.cpp
+++ kmp.cpp
@@ -1,31 +1,15 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-vector<int> p;
-
-void generate(string &s){
-  // O(n)
-  // p[i] is the length of longest prefix 
-  // in [0,i) which is also a suffix of this substring
-  int n=s.size();
-  p.resize(n+1);
-  p[0]=-1;
-  for(int i=1; i<=n; i++){
-    int k=p[i-1];
-    while(k>=0 && s[k]!=s[i-1]){
-      k=p[k];
-    }
-    p[i]=k+1;
-  }
-}
-
-int main(){
-    ios::sync_with_stdio(0), cin.tie(0);
-
-    string s,t;
-    cin>>s>>t;
-    string N=t+'!'+s;
-    generate(N);
-    for(int i=0; i<=n; i++) cout<<p[i]<<' ';
-    return 0;
+vector<int> kmp(string &s){
+        // p[i] is the length of longest prefix 
+        // in [0,i) which is also a suffix of this substring
+        const int n=s.size();
+        vector<int> p(n+1);
+        p[0]=-1;
+        for(int i=1; i<=n; ++i){
+                int k=p[i-1];
+                while(k>=0 && s[k]!=s[i-1]){
+                        k=p[k];
+                }
+                p[i]=k+1;
+        }
+        return p;
 }

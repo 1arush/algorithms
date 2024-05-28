be358bbf40f1b04c6fe6f0dc2a7bab4cbaef73b2
Rushil Khullar <105356056+1arush@users.noreply.github.com>
2024-05-28 08:06:21 +0530

    Updated bit.cpp

--- bit.cpp
+++ bit.cpp
@@ -1,19 +1,21 @@
-// only used for 1-indexing
-// if using for 0-base, add 1 to all indices
+// originally used for 1-indexing
+// if using for 0-base, ++ before query
+// query like : get(x) = sum[0,x]
 
 const int mxN=2e5+5;
+
 int fen[mxN], n;
 
-void init(int sz){
+void reset(int sz){
         for(int i=0; i<=sz; ++i) fen[i]=0;
 }
 
 void get(int x){
         int ret=0;
-        for(; x>0; x-=x&-x) ret+=fen[x];
+        for(++x; x>0; x-=x&-x) ret+=fen[x];
         return ret;
 } 
 
 void add(int x, int v){
-        for(; x<=n; x+=x&-x) fen[x]+=v;
+        for(++x; x<=n; x+=x&-x) fen[x]+=v;
 }

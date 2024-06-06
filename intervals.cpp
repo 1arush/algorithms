8bab719953421f412ed828393ed8dce75c8b11e1
Rushil <105356056+1arush@users.noreply.github.com>
2024-06-07 04:51:35 +0530

    Updated intervals.cpp

--- intervals.cpp
+++ intervals.cpp
@@ -1,39 +1,44 @@
 /*
-Given an array of numbers, we want to remove and add some elements,
+given an array of numbers, we want to remove and add some elements,
 and query the maximal absolute difference among adjacent elements in
-the sorted array. 
+the sorted array
 
-Note : Please handle the cases where N=1, it often leads to RTE.
+handle the cases where n=1, often leads to RTE
 */
-auto it=ms.lower_bound(a[i]);
-// remove a[i]
+
+// ms := multiset that stores points
+// d  := multiset that stores distances between adjacent points
+
+// remove x from the set
+auto it=ms.lower_bound(x);
 if(it==ms.begin() && ms.size()>=2){
-    auto pt=next(it);
-    d.erase(d.find(*pt-*it));
+        // if smallest element
+        d.erase(d.find(*pt-*next(it)));
 }
 else if(next(it)==ms.end() && ms.size()>=2){
-    auto pt=prev(it);
-    d.erase(d.find(*it-*pt));
+        // if largest element
+        d.erase(d.find(*it-*prev(it)));
 }
 else if(ms.size()>1){
-    auto pt=prev(it), nt=next(it);
-    d.erase(d.find(*it-*pt));
-    d.erase(d.find(*nt-*it));
-    d.insert(*nt-*pt);
+        auto pt=prev(it), nt=next(it);
+        d.erase(d.find(*it-*pt));
+        d.erase(d.find(*nt-*it));
+        d.insert(*nt-*pt);
 }
-ms.erase(ms.find(a[i]));
-// add x
+ms.erase(ms.find(x));
+
+// add x to the set
 it=ms.upper_bound(x);
 if(it==ms.end()){
-    d.insert(x-*prev(it));
+        d.insert(x-*prev(it));
 }
 else if(it==ms.begin()){
-    d.insert(*it-x);
+        d.insert(*it-x);
 }
 else if(ms.size()>1){
-    auto pt=prev(it);
-    d.erase(d.find(*it-*pt));
-    d.insert(x-*pt);
-    d.insert(*it-x);
+        auto pt=prev(it);
+        d.erase(d.find(*it-*pt));
+        d.insert(x-*pt);
+        d.insert(*it-x);
 }
 ms.insert(x);

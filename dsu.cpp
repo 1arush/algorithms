c88382bd91d9b11ed1140970845b7b0581299d6b
Rushil <105356056+1arush@users.noreply.github.com>
2024-06-18 17:58:23 +0530

    Updated dsu.cpp

--- dsu.cpp
+++ dsu.cpp
@@ -8,13 +8,15 @@ struct DSU {
         int leader(int a){
                 return (a==p[a] ? a : p[a]=leader(p[a]));
         }
-        bool unite(int a, int b){
+        bool same(int a, int b){
+                a=leader(a), b=leader(b);
+                return a==b;
+        }
+        void unite(int a, int b){  // union by size
                 a=leader(a), b=leader(b);
                 if(sz[a]<sz[b]) swap(a,b);
                 if(a!=b){
                         p[b]=a, sz[a]+=sz[b];
-                        return 1;
                 }
-                return 0;
         }
 };

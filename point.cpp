194dc22a02bf770b61f75c52b20a879926aea5d1
Rushil <105356056+1arush@users.noreply.github.com>
2024-06-20 20:46:04 +0530

    Updated point.cpp

--- point.cpp
+++ point.cpp
@@ -1,12 +1,27 @@
 struct Pt {
         int x, y;
-        Pt(int a=0, int b=0){ x=a, y=b; }
-        void read(){ cin>>x>>y; }
-        Pt operator -(const Pt &b) const { return Pt(x-b.x,y-b.y); }
-        Pt operator +(const Pt &b) const { return Pt(x+b.x,y+b.y); }
-        void operator -=(const Pt &b){ x-=b.x, y-=b.y; }
-        void operator +=(const Pt &b){ x+=b.x, y+=b.y; }
-        int dot(const Pt &b) const { return x*b.x+y*b.y; }
-        int cross(const Pt &b) const { return x*b.y-y*b.x; }
-        int triangle(const Pt &a, const Pt &b) const { return (a-*this).cross(b-*this); }
+        Pt(int a=0, int b=0){ 
+                x=a, y=b; 
+        }
+        void read(){ 
+                cin>>x>>y; 
+        }
+        Pt operator -(const Pt &b) const { 
+                return Pt(x-b.x,y-b.y); 
+        }
+        Pt operator +(const Pt &b) const { 
+                return Pt(x+b.x,y+b.y); 
+        }
+        bool operator <(const Pt &b) const {
+                return make_pair(x,y)<make_pair(b.x,b.y);
+        }
+        int dot(const Pt &b) const { 
+                return x*b.x+y*b.y; 
+        }
+        int cross(const Pt &b) const { 
+                return x*b.y-y*b.x; 
+        }
+        int triangle(const Pt &a, const Pt &b) const { 
+                return (a-*this).cross(b-*this);  
+        }
 };

49e68268397191ba3550111903b0e3353e2087fd
Rushil <105356056+1arush@users.noreply.github.com>
2024-07-25 09:51:33 +0530

    Updated lazy-segtree.cpp

--- lazy-segtree.cpp
+++ lazy-segtree.cpp
@@ -35,7 +35,7 @@ struct Segtree {
                 build(2*x+2,m,rx);
                 tree[x]=merge(tree[2*x+1],tree[2*x+2]);
         }
-        void modify(int l, int r, int v, int x, int lx, int rx){
+        void chg(int l, int r, int v, int x, int lx, int rx){
                 push(x,lx,rx); 
                 if(rx<=l || lx>=r) return;
                 if(lx>=l && rx<=r){
@@ -44,23 +44,23 @@ struct Segtree {
                         return;
                 }
                 int m=(lx+rx)/2;
-                modify(l,r,v,2*x+1,lx,m);
-                modify(l,r,v,2*x+2,m,rx);
+                chg(l,r,v,2*x+1,lx,m);
+                chg(l,r,v,2*x+2,m,rx);
                 tree[x]=apply(merge(tree[2*x+1],tree[2*x+2]),lazy[x],rx-lx);
         }
-        int calc(int l, int r, int x, int lx, int rx){
+        int ask(int l, int r, int x, int lx, int rx){
                 push(x,lx,rx);
                 if(rx<=l || lx>=r) return NEUTRAL;
                 if(lx>=l && rx<=r) return tree[x];
                 int m=(lx+rx)/2;
-                auto m1=calc(l,r,2*x+1,lx,m);
-                auto m2=calc(l,r,2*x+2,m,rx);
+                auto m1=ask(l,r,2*x+1,lx,m);
+                auto m2=ask(l,r,2*x+2,m,rx);
                 return apply(merge(m1,m2),lazy[x],min(rx,r)-max(lx,l));
         }
-        void modify(int l, int r, int v){
-                modify(l,r+1,v,0,0,sz);
+        void chg(int l, int r, int v){
+                chg(l,r+1,v,0,0,sz);
         }
-        int calc(int l, int r){
-                return calc(l,r+1,0,0,sz);
+        int ask(int l, int r){
+                return ask(l,r+1,0,0,sz);
         }
 };

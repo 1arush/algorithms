a884f13e5136986f568eb9dc30a166686bc1dbc7
Rushil <105356056+1arush@users.noreply.github.com>
2024-08-01 16:12:43 +0530

    Updated binpow.cpp

--- binpow.cpp
+++ binpow.cpp
@@ -1,12 +1,14 @@
+const int mod=1e9+7;
+
 int binpow(int x, int n){
         int ret=1;
         while(n){
-                if(n&1) ret=ret*x%M;
-                x=x*x%M, n>>=1;
+                if(n&1) ret=ret*x%mod;
+                x=x*x%mod, n>>=1;
         }
         return ret;
 }
 
-int inverse(int x){  // prime M
-        return binpow(x,M-2,M);
+int inverse(int x){  // mod is prime
+        return binpow(x,mod-2);
 }

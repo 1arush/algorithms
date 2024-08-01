fae2e1957bb7319bda0eed75e1cf4a3d816fea36
Rushil <105356056+1arush@users.noreply.github.com>
2024-08-01 16:11:12 +0530

    Updated bit.cpp

--- bit.cpp
+++ bit.cpp
@@ -1,10 +1,9 @@
-// use with 0-base (works 1-base internally)
-// get(x)=sum of [0..x-1]
+// get(x)=sum of [1..x]
 // add(x,v)=add v to a[x]
 
-const int MXN=2e5+5;
+const int nax=2e5+5;
 
-int fen[MXN], n;
+int fen[nax], n;
 
 void reset(int sz){
         for(int i=0; i<=sz; ++i) fen[i]=0;
@@ -12,10 +11,10 @@ void reset(int sz){
 
 int get(int x){
         int ret=0;
-        for(++x; x>0; x-=x&-x) ret+=fen[x];
+        for(; x>0; x-=x&-x) ret+=fen[x];
         return ret;
 } 
 
 void add(int x, int v){
-        for(++x; x<=n; x+=x&-x) fen[x]+=v;
+        for(; x<=n; x+=x&-x) fen[x]+=v;
 }

5e251426ef76791fd9584e1fd99e03265a9ccade
Rushil <105356056+1arush@users.noreply.github.com>
2024-09-13 10:59:11 +0530

    Updated factorial.cpp

--- factorial.cpp
+++ factorial.cpp
@@ -1,10 +1,10 @@
-const int mod=1e9+7, nax=1e5+5;
+const int mod=1e9+7, maxn=1e5+5;
 
-vector<int> fac(nax), inv(nax), ifac(nax);
+vector<int> fac(maxn), inv(maxn), ifac(maxn);
 
 void factorial(){
         fac[0]=inv[1]=ifac[0]=1;
-        for(int i=1; i<nax; ++i){
+        for(int i=1; i<maxn; ++i){
                 fac[i]=fac[i-1]*i%mod;
                 if(i>1) inv[i]=(mod-(mod/i)*inv[mod%i]%mod)%mod;
                 ifac[i]=ifac[i-1]*inv[i]%mod;

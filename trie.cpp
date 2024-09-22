a8947dae07367a319ea65ec551b9936972ced533
Rushil <105356056+1arush@users.noreply.github.com>
2024-09-22 18:31:49 +0530

    Updated trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,26 +1,22 @@
-const int N=2e5*32+500, A=2;
+const int N=1e6+6, A=11;
 // max nodes and alphabet size
-
+ 
 int nxt[N][A], cnt[N], idx;
-
-void insert(int x, int v){
+ 
+void insert(vector<int>& x, int v){
         int p=0;
-        for(int i=31; ~i; --i){
-                int u=(x>>i)&1;
+        for(int u:x){
                 if(!nxt[p][u]) nxt[p][u]=++idx;
                 cnt[p]+=v, p=nxt[p][u];
         }
         cnt[p]+=v;
 }
-
-int query(int x){
-        int p=0, ret=0;
-        for(int i=31; ~i; --i){
-                int u=(x>>i)&1;
-                if(nxt[p][u] && cnt[nxt[p][u]]){
-                        ret+=(1<<i), p=nxt[p][u];
-                }
-                else p=nxt[p][!u];
+ 
+int query(vector<int>& x){
+        int p=0;
+        for(int u:x){
+                if(!nxt[p][u]) return (int)0;
+                p=nxt[p][u];
         }
-        return ret;
+        return cnt[p];
 }
